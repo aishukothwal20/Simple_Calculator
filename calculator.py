03af1bb9524ba41172a9802923eaa98e84d19280
aishukothwal20 <aishwaryakothwal20@gmail.com>
2025-07-03 14:49:08 +0530

    Added multiply function

--- calculator.py
+++ calculator.py
@@ -7,6 +7,10 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def multiply(a, b):
+    return a / b
+
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(10, 4))
 print("Multiplication:", multiply(6, 7))
+print("Divide:", divide(10, 5))

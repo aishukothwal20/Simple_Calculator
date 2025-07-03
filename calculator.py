48361906be74ff9b30b7f105069c72304d1c6808
aishukothwal20 <aishwaryakothwal20@gmail.com>
2025-07-03 15:04:29 +0530

    Resolved merge conflict: added divide function and updated print statements

--- calculator.py
+++ calculator.py
@@ -10,7 +10,7 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
-print("Addition:", add(5, 3))
+print("Addition:", add(10, 3))
 print("Subtraction:", subtract(10, 4))
 print("Multiplication:", multiply(6, 7))
 print("Divide:", divide(10, 5))
